588961fbb4fb395d25f46e08b3945cf59623c3a6
Zedong <peng_zedong@126.com>
2020-09-06 14:17:18 +0800

    test

--- pyomo/contrib/mindtpy/MindtPy.py
+++ pyomo/contrib/mindtpy/MindtPy.py
@@ -119,6 +119,7 @@ class MindtPySolver(object):
             add_baron_cuts(model)
             config.logger.info(
                 "Use the fbbt to tighten the bounds of variables")
+            model.pprint()
 
         solve_data.original_model = model
         solve_data.working_model = model.clone()
